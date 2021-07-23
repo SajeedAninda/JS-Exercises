6265d8ca6714ea891e0baa3a814545c3bd476607
Sajeed Aninda <87448831+SajeedAninda@users.noreply.github.com>
2021-07-24 02:31:20 +0600

    Create Multiplication and Division.js

---
+++ Multiplication and Division.js
@@ -0,0 +1,10 @@
+// Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
+
+let num1=prompt("Please Enter the first Number: ");
+let num2=prompt("Please Enter the second Number: ");
+
+let multiplication=num1*num2;
+let division=num1/num2;
+
+console.log(`The multiplied value is ${multiplication}`);
+console.log(`The division value is ${division}`);
