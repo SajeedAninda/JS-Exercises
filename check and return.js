45dc08bb30465fb48f3b8d1a5e19a7105208bd3b
Sajeed Aninda <87448831+SajeedAninda@users.noreply.github.com>
2021-07-24 03:12:08 +0600

    Create check and return.js

---
+++ check and return.js
@@ -0,0 +1,10 @@
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+
+function check(a,b){
+  console.log(a==50||b==50||a+b==50);
+}
+
+check(50,50);
+check(22,28);
+check(12,56);
+check(50,26);
